0e5cd6cc99e5333bcfda806d507b6efbd43c181b
Soorej Jones Pothoor <soorejjones@gmail.com>
2016-02-29 22:10:41 +0530

    Modifications to ux

--- testFile.py
+++ testFile.py
@@ -9,18 +9,25 @@ import sys
 dbObj = dB.DatabaseLayer(dB.dbName)
 
 class MainForm(QtGui.QMainWindow, window.Ui_MainWindow):
+    widgetDict = {'createClient':0,'confirmClient':1}
     def __init__(self, parent = None):
         super(MainForm,self).__init__(parent)
         #self.ui = 
         self.setupUi(self)
-        QtCore.QObject.connect(self.pushButtonSubmit,QtCore.SIGNAL("clicked()"),self.buttonclickSubmit)
+        self.stackedWidget.setCurrentIndex(self.widgetDict['createClient'])
+        QtCore.QObject.connect(self.pushButtonNext,QtCore.SIGNAL("clicked()"),self.buttonclickSubmit)
+        QtCore.QObject.connect(self.pushButtonBack, QtCore.SIGNAL("clicked()"),self.buttonclickBack)
     def buttonclickSubmit(self):
         name = self.lineEditName.text()
         phoneNumber = self.lineEditPhone.text()
-        notes = self.textEditNotes.toPlainText()
+        notes = self.textEditInfo.toPlainText()
+        self.stackedWidget.setCurrentIndex(self.widgetDict['confirmClient'])
+        self.textBrowserConfirmClientInfo.setText("Name:\tPhone:\tNotes\n{}\t{}\t{}".format(name,phoneNumber,notes))
         #dbObj.addService(name,phoneNumber,notes)  #Add after completing ui
-        self.addService.hide()
 
+        
+    def buttonclickBack(self):
+        self.stackedWidget.setCurrentIndex(self.stackedWidget.currentIndex()-1)
         
 def main():
     app = QtGui.QApplication(sys.argv)
